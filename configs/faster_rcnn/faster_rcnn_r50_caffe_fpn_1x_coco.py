3e07555f6c8af50c2cffdb42fc9d4c5197f5f631
raghava-koganti <63447408+raghava-koganti@users.noreply.github.com>
2022-06-25 00:11:02 +0530

    Update faster_rcnn_r50_caffe_fpn_1x_coco.py
    changed the img_scale from (1333, 800) to (8192, 8192) for #GDSC5

--- configs/faster_rcnn/faster_rcnn_r50_caffe_fpn_1x_coco.py
+++ configs/faster_rcnn/faster_rcnn_r50_caffe_fpn_1x_coco.py
@@ -13,7 +13,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='Resize', img_scale=(8192, 8192), keep_ratio=True), #changed from (1333, 800)
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -24,7 +24,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
+        img_scale=(8192, 8192), #changed from (1333, 800)
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
